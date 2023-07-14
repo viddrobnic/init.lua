2a1c955cdaec90d3ea1620b32722c76ca21b9319
Vid Drobnic <vid@zerodays.dev>
2023-07-14 11:38:35 +0200

    add git blame to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,19 +1,41 @@
 -- Status line
 return {
   'nvim-lualine/lualine.nvim',
-  opts = {
-    options = {
-      theme = 'catppuccin-macchiato',
-      icons_enabled = true,
-    },
-    sections = {
-      lualine_c = {
-        {
-          'filename',
-          file_status = true,
-          path = 1,
+  dependencies = {
+    'f-person/git-blame.nvim',
+  },
+  config = function()
+    local git_blame = require('gitblame')
+    vim.g.gitblame_display_virtual_text = 0
+    vim.g.gitblame_date_format = '%r'
+    vim.g.gitblame_message_template = '<author> • <date>'
+
+    require('lualine').setup({
+      options = {
+        theme = 'catppuccin-macchiato',
+        icons_enabled = true,
+      },
+      sections = {
+        lualine_c = {
+          {
+            'filename',
+            file_status = true,
+            path = 1,
+          },
         },
-      }
-    }
-  }
+        lualine_x = {
+          {
+            git_blame.get_current_blame_text,
+            cond = git_blame.is_blame_text_available,
+            separator = '',
+            padding = 3,
+            color = { fg = '#6e738d' },
+          },
+          'encoding',
+          'fileformat',
+          'filetype',
+        },
+      },
+    })
+  end
 }
