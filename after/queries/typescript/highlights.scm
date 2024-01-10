c22782ba8189dd332ab8d84cee1787b2b93d6390
Vid Drobnič <vid.drobnic@pm.me>
2024-01-10 14:31:32 +0100

    add typescript spelling config

---
+++ after/queries/typescript/highlights.scm
@@ -0,0 +1,13 @@
+; extends
+(type_alias_declaration 
+  name: (type_identifier) @spell)
+
+(variable_declarator 
+  name: (identifier) @spell)
+
+(interface_declaration
+  name: (type_identifier) @spell)
+
+(function_declaration
+  name: (identifier) @spell)
+
