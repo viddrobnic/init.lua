b7b9405f66f490fc1343545e85eedaa9198cf112
Vid Drobnič <vid.drobnic@pm.me>
2024-02-20 18:54:01 +0100

    improve cmp suggestions

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,15 +1,22 @@
 return {
   'hrsh7th/nvim-cmp',
   dependencies = {
+    -- Copilot as snippets
+    'zbirenbaum/copilot-cmp',
+
     -- Snippet Engine & its associated nvim-cmp source
     'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
 
     -- Adds LSP completion capabilities
     'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-path',
 
     -- Adds a number of user-friendly snippets
     'rafamadriz/friendly-snippets',
+
+    -- Icons in cmp menu
+    'onsails/lspkind.nvim',
   },
   config = function()
     -- Set keymap for autocomplete
@@ -18,7 +25,23 @@ return {
     require('luasnip.loaders.from_vscode').lazy_load()
     luasnip.config.setup({})
 
+    require("copilot_cmp").setup()
+
+    local has_words_before = function()
+      if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+    end
+
+    local lspkind = require('lspkind')
     cmp.setup({
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = "symbol_text",
+          max_width = 50,
+          symbol_map = { Copilot = "" }
+        })
+      },
       snippet = {
         expand = function(args)
           luasnip.lsp_expand(args.body)
@@ -29,26 +52,32 @@ return {
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-k>'] = cmp.mapping.select_prev_item(),
         ['<C-j>'] = cmp.mapping.select_next_item(),
-        ['<Tab>'] = cmp.config.disable,
-        ['<S-Tab>'] = cmp.config.disable,
-        ['<C-f>'] = cmp.mapping(function(fallback)
-          if luasnip.expand_or_locally_jumpable() then
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ["<Tab>"] = vim.schedule_wrap(function(fallback)
+          if cmp.visible() and has_words_before() then
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+          elseif luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
           else
             fallback()
           end
         end, { 'i', 's' }),
-        ['<C-b>'] = cmp.mapping(function(fallback)
-          if luasnip.locally_jumpable() then
-            luasnip.jump(1)
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
           else
             fallback()
           end
         end, { 'i', 's' }),
       },
       sources = {
-        { name = 'nvim_lsp' },
+        { name = 'copilot' },
         { name = 'luasnip' },
+        { name = 'nvim_lsp' },
+        { name = 'path' },
       },
       window = {
         completion = cmp.config.window.bordered(),
--- lua/set.lua
+++ lua/set.lua
@@ -1,4 +1,4 @@
-vim.opt.guicursor = ''
+vim.opt.guicursor = 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250'
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
