34b7227a8b220b41317ec49b9d84a1ee4158e77c
Vid Drobnič <vid.drobnic@pm.me>
2024-06-30 17:43:14 +0200

    feat: change harpoon shortcuts

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -13,10 +13,11 @@ return {
         { desc = '[H]arpoon toggle m[e]nu' })
       vim.keymap.set('n', '<leader>ha', function() harpoon:list():add() end, { desc = '[H]arpoon [a]dd file' })
 
-      vim.keymap.set('n', '<S-h>', function() harpoon:list():select(1) end)
-      vim.keymap.set('n', '<S-j>', function() harpoon:list():select(2) end)
-      vim.keymap.set('n', '<S-k>', function() harpoon:list():select(3) end)
-      vim.keymap.set('n', '<S-l>', function() harpoon:list():select(4) end)
+      vim.keymap.set('n', '<leader>1', function() harpoon:list():select(1) end)
+      vim.keymap.set('n', '<leader>2', function() harpoon:list():select(2) end)
+      vim.keymap.set('n', '<leader>3', function() harpoon:list():select(3) end)
+      vim.keymap.set('n', '<leader>4', function() harpoon:list():select(4) end)
+      vim.keymap.set('n', '<leader>5', function() harpoon:list():select(5) end)
     end,
   },
 }
