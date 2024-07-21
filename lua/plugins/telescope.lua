91ad52ddc7f397326ca6beeff79a1ac476b76ce8
Vid Drobnič <me@viddrobnic.com>
2024-07-21 20:18:46 +0200

    feat: add resume search command

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -49,6 +49,7 @@ return {
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
